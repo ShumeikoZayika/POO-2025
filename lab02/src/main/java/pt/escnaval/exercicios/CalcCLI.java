f5ada719f85e6e936de5209108c75f08682ba9f1
Daniel Zayika <zayika@MacBook-Pro-de-Daniel.local>
2025-10-01 11:59:37 +0100

    Initial commit with all files created

---
+++ lab02/src/main/java/pt/escnaval/exercicios/CalcCLI.java
@@ -0,0 +1,5 @@
+package pt.escnaval.exercicios;
+
+public class CalcCLI {
+    // ...implementação...
+}
---
+++ lab02/src/main/java/pt/escnaval/exercicios/ConversorUnidades.java
@@ -0,0 +1,5 @@
+package pt.escnaval.exercicios;
+
+public class ConversorUnidades {
+    // ...implementação...
+}
