f43da497d22a71066d076d778266b66ed3fa7434
Daniel Zayika <zayika@MacBook-Pro-de-Daniel.local>
2025-10-15 11:43:28 +0100

    save: current workspace state

--- lab04/src/main/java/pt/escnaval/exercicios/ExemploLab04.java
+++
@@ -1,7 +0,0 @@
-package pt.escnaval.exercicios;
-
-public class ExemploLab04 {
-    public static void main(String[] args) {
-        System.out.println("Lab04: Estrutura criada com sucesso.");
-    }
-}
